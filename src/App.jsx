7ee679c0cfbc16ef4b304113ab2c8853961c96fa
Felipe Schulz <36828158+FelipeSchulzLuz@users.noreply.github.com>
2021-06-23 22:12:44 -0300

    componente sempre sendo renderizado

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,22 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./paginas/Home";
 import Sobre from "./paginas/Sobre";
 import Pagina404 from "./paginas/Pagina404";
-
+import Cabecalho from "./components/Cabecalho";
 
 function App() {
   return (
     <Router>
+      <Cabecalho />
       <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/sobre">
-        <Sobre />
-      </Route>
-      <Route>
-        <Pagina404/>
-      </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/sobre">
+          <Sobre />
+        </Route>
+        <Route>
+          <Pagina404 />
+        </Route>
       </Switch>
     </Router>
   );
