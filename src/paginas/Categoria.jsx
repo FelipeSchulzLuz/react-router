5a1f3f3a80d7171a358a8d4ecd6195523c4b5390
Felipe Schulz <Schulz.neo@gmail.com>
2021-06-28 21:29:58 -0300

    implementando subcategorias e add useEffect

--- src/paginas/Categoria.jsx
+++ src/paginas/Categoria.jsx
@@ -1,22 +1,52 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import "../assets/css/blog.css";
+import React, { useEffect, useState } from "react";
 import ListaCategorias from "../components/ListaCategorias";
 import ListaPost from "../components/ListaPost";
-import { Route, useRouteMatch } from "react-router-dom";
+import SubCategoria from "./SubCategoria";
+import {
+  Route,
+  Link,
+  useRouteMatch,
+  Switch,
+  useParams,
+} from "react-router-dom";
+import { busca } from "../api/api";
+import "../assets/css/blog.css";
 
 const Categoria = () => {
   const { id } = useParams();
-  const { path } = useRouteMatch();
+  const { url, path } = useRouteMatch();
+  const [subcategorias, setSubCategorias] = useState([]);
+
+  useEffect(() => {
+    busca(`/categorias/${id}`, (categoria) => {
+      setSubCategorias(categoria.subcategorias);
+    });
+  }, [id]);
+
   return (
     <>
       <div className="container">
         <h2 className="titulo-pagina">Pet Notícias</h2>
       </div>
-      <ListaCategorias></ListaCategorias>
-      <Route exact path={`${path}/`}>
-        <ListaPost url={`/posts?categoria=${id}`}></ListaPost>
-      </Route>
+      <ListaCategorias />
+      <ul className="lista-categorias container flex">
+        {subcategorias.map((subcategoria) => (
+          <li
+            className={`lista-categorias__categoria lista-categorias__categoria--${id}`}
+            key={subcategoria}
+          >
+            <Link to={`${url}/${subcategoria}`}>{subcategoria}</Link>
+          </li>
+        ))}
+      </ul>
+      <Switch>
+        <Route exact path={`${path}/`}>
+          <ListaPost url={`/posts?categoria=${id}`}/>
+        </Route>
+        <Route path={`${path}/:subcategoria`}>
+          <SubCategoria />
+        </Route>
+      </Switch>
     </>
   );
 };
